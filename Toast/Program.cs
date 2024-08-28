a5b0fe38f9510e26df6bfcc154a9989e34734bb1
tdonnelly1102 <58100933+tdonnelly1102@users.noreply.github.com>
2024-08-28 16:42:35 +0000

    Created a console based project

---
+++ Toast/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+for(int i = 0; i < 10; i++){
+    Console.WriteLine("i = " + i);
+}
+
+int x = 2;
+string name = "Tom";
+double pi = 3.1415;
+bool yesno = false;
+
+if(x == 2) Console.WriteLine("x = " + x.ToString());
+Console.WriteLine(name.ToLower());
+Console.WriteLine("pi = " + pi.ToString("N2"));
+if(yesno) Console.WriteLine("true");
+else Console.WriteLine("false");
